598284ce1249a8a5b5928f599a7bc6ac98168678
Devesh Gupta <contact.devesh37@gmail.com>
2026-07-17 21:38:42 +0000

    advance CI

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 VERSION = "1.0.0"
 
-
 @app.get("/")
 def home():
     return jsonify({
@@ -12,20 +11,14 @@ def home():
         "version": VERSION
     })
 
-
 @app.get("/health")
 def health():
     return jsonify({
         "status": "healthy"
     })
 
-
 @app.get("/version")
 def version():
     return jsonify({
         "version": VERSION
     })
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
