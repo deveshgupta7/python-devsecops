2f884af53712f9103b662b64a19b36d2e71c807e
Devesh Gupta <contact.devesh37@gmail.com>
2026-07-17 21:44:49 +0000

    added more test and format

--- app.py
+++ app.py
@@ -4,21 +4,17 @@ app = Flask(__name__)
 
 VERSION = "1.0.0"
 
+
 @app.get("/")
 def home():
-    return jsonify({
-        "application": "Python Demo",
-        "version": VERSION
-    })
+    return jsonify({"application": "Python Demo", "version": VERSION})
+
 
 @app.get("/health")
 def health():
-    return jsonify({
-        "status": "healthy"
-    })
+    return jsonify({"status": "healthy"})
+
 
 @app.get("/version")
 def version():
-    return jsonify({
-        "version": VERSION
-    })
+    return jsonify({"version": VERSION})
